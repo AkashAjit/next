d1479fc7bd127303af6e1df3d201d348d458a59c
Akash Ajith <akash.ajith3@gmail.com>
2023-01-12 02:59:27 +0530

    firstcommit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import '../styles/globals.css'
-
+import React,{useEffect} from 'react'
 export default function App({ Component, pageProps }) {
+   useEffect(() => {
+    const use = async () => {
+      (await import("tw-elements")).default;
+    };
+    use();
+  }, []);
   return <Component {...pageProps} />
 }
